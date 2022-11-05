a0f5e3df599dd73b2859c95c34834bae7ff2224a
perumalswamy raju <perumalswamy.raju@redfin.com>
2022-11-04 19:39:52 -0700

    webserver

--- Single_server/main.tf
+++ Single_server/main.tf
@@ -6,7 +6,15 @@ resource "aws_instance" "example" {
   ami           =  "ami-0fb653ca2d3203ac1" 
   instance_type = "t2.micro"
   tags = {
-    #Name = "example-instance-name"
-    Name = var.name
+    Name = "example-instance-name"
   }
+
+         user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
 }
